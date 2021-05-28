754188f2b7f5c76bcade147cac182e18288c93c4
Sk Md Rakib <skmdrakibcse@gmail.com>
2021-05-28 15:04:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from "react";
+
+import Table from "./Component/table";
 
 function App() {
+
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        
+        // First group columns
+        columns: [
+          {
+            Header: "Mobile",
+            accessor: "show.mobile"
+          },
+          {
+            Header: "Id",
+            accessor: "show.id"
+          },
+          {
+            Header: "Earning",
+            accessor: "show.earning"
+          },
+          {
+            Header: "Action",
+            accessor: "show.action"
+          }
+        ]
+      },
+      
+    ],
+    [] //put the data into the cells in this array []
+  );
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
